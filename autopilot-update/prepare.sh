6a5e04f9ff7f57cc31b1147ca4c808537f265855
Jussi Nummelin <jnummelin@mirantis.com>
2024-02-16 12:27:23 +0200

    Add "airgap" file server using Multipass
    Also tweak autopilot demo to utilize the local file server for binary and airgap image downloads
    
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

--- autopilot-update/prepare.sh
+++ autopilot-update/prepare.sh
@@ -7,13 +7,14 @@ source ../helpers.sh
 # Setup SSH public key in clout-init data using envsubst
 SSH_PUBLIC_KEY=$(cat ~/.ssh/id_rsa.pub) envsubst < ci.yaml.tmpl > ci.yaml
 
+file_server_ip=$(ipAddress k0s-files)
 
 # Launch VMs
 for name in k0sctl-controller k0sctl-worker1 k0sctl-worker2;do
   multipass launch -n $name --cloud-init ci.yaml
 done
 
-CONTROLLER_IP=`ipAddress k0sctl-controller` WORKER1_IP=`ipAddress k0sctl-worker1` WORKER2_IP=`ipAddress k0sctl-worker2` envsubst < k0sctl.yaml.tmpl > k0sctl.yaml
+CONTROLLER_IP=`ipAddress k0sctl-controller` WORKER1_IP=`ipAddress k0sctl-worker1` WORKER2_IP=`ipAddress k0sctl-worker2` FILE_SERVER_IP=$file_server_ip envsubst < k0sctl.yaml.tmpl > k0sctl.yaml
 
 k0sctl apply -c k0sctl.yaml
 
@@ -26,4 +27,10 @@ for name in k0sctl-worker1 k0sctl-worker2;do
   multipass exec $name -- sudo systemctl daemon-reload
 done
 
-k0sctl apply -c k0sctl.yaml > kube.config
+k0sctl kubeconfig -c k0sctl.yaml > kube.config
+
+echo "k0sctl cluster is ready, kubeconfig at kube.config"
+echo "To destroy the cluster, run ./destroy.sh"
+
+# Replace the file server IP in the plan.yaml
+FILE_SERVER_IP=$file_server_ip envsubst < plan.yaml.tmpl > plan.yaml
