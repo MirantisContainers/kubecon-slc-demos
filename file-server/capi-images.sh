c96aaa84a93fd960b43135d16808cf77e4c1aa5f
Jussi Nummelin <jnummelin@mirantis.com>
2024-02-19 11:52:06 +0200

    Airgap remote-machines demo
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

---
+++ file-server/capi-images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+images="
+quay.io/metallb/speaker:v0.14.3
+registry.k8s.io/cluster-api/cluster-api-controller:v1.6.1
+registry.k8s.io/cluster-api/kubeadm-control-plane-controller:v1.6.1
+registry.k8s.io/cluster-api/kubeadm-bootstrap-controller:v1.6.1
+registry.k8s.io/metrics-server/metrics-server:v0.6.4
+quay.io/metallb/controller:v0.14.3
+gcr.io/kubebuilder/kube-rbac-proxy:v0.13.1
+quay.io/jetstack/cert-manager-controller:v1.11.1
+quay.io/jetstack/cert-manager-webhook:v1.11.1
+quay.io/jetstack/cert-manager-cainjector:v1.11.1
+registry.k8s.io/pause:3.9
+"
+
+for image in $images; do
+  docker pull $image
+done
+
+docker save $images -o files/images/capi-images.tar
--- file-server/download.sh
+++ file-server/download.sh
@@ -4,3 +4,4 @@ set -e
 
 wget -i images.txt -P files/images
 wget -i bins.txt -P files/bins
+wget -i manifests.txt -P files/manifests
